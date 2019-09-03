6f42d0bd1af5289af8eaab53f97aded1be79eeec
zhuchenchen <550400438@qq.com>
2019-09-03 09:20:28 +0800

    test

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="app">
+    <input v-model="nums">
+    <Counter @count="count" v-for="n in getNum(nums)" v-bind:key="n" v-bind:msg="n"/>
+    <span>Sum:{{sum}}</span>
+  </div>
+</template>
+
+<script>
+import Counter from './components/Counter.vue'
+export default {
+  name: 'App',
+  components: {
+    Counter
+  },
+  methods: {
+    getNum: function (e) {
+      let temp = 0
+      let reg = /^[0-9]\d*$/
+      if (!reg.test(e)) {
+        return temp
+      }
+      temp = parseInt(e)
+      return temp
+    },
+    count: function (e) {
+      this.sum = this.sum + e
+    }
+  },
+  data () {
+    return {
+      nums: 20,
+      sum: 0
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
